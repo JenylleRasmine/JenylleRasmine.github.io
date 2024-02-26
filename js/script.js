31d3b318d89690b4e54a16f660ebdb211407e44a
Jenylle Macabugao <158466744+JenylleRasmine@users.noreply.github.com>
2024-02-26 17:41:24 +0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ const body = document.querySelector('body');
 const header = document.querySelector('header');
 const overlay = document.querySelector('.overlay');
 const fadeELems = document.querySelectorAll('.has-fade');
+const menu = document.querySelector('.header__menu');
 
 btnHamburger.addEventListener('click', function(){
     console.log('open hamburger');
@@ -26,6 +27,32 @@ btnHamburger.addEventListener('click', function(){
         });
     }
 });
+
+overlay.addEventListener('click', function(){
+    console.log('close menu via clicking overlay');
+    //Close menu via clicking overlay
+    if (header.classList.contains('open')){
+        body.classList.remove('noscroll');
+        header.classList.remove('open');
+        fadeELems.forEach(element => {
+            element.classList.remove('fade-in');
+            element.classList.add('fade-out');
+        });
+    }
+})
+
+menu.addEventListener('click', function(){
+    console.log('menu closes when menu is clicked');
+    //menu closes when menu is clicked
+    if (header.classList.contains('open')){
+        body.classList.remove('noscroll');
+        header.classList.remove('open');
+        fadeELems.forEach(element => {
+            element.classList.remove('fade-in');
+            element.classList.add('fade-out');
+        });
+    }
+})
 
 //Expand More
 const btnExpand = document.querySelectorAll('#btnExpand');
